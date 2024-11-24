0f359fbce1e397c53b487facc98f6181f12ae9d9
nishamashhood <mashhoodnisha@gmail.com>
2024-11-24 14:27:06 +0530

    daisy Ui setup

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,15 @@
 const App = () => {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </h1>
+  );
 };
 
 export default App;
