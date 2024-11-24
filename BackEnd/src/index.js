4fb0b9ce351f5b0a3a28c9495703427230698725
nishamashhood <mashhoodnisha@gmail.com>
2024-11-24 20:33:49 +0530

    signup page designed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import { connectDb } from './lib/db.js';
 dotenv.config();
 const PORT = process.env.PORT
@@ -9,7 +11,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`Server is listening to port :${PORT}`);
